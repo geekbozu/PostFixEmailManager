76ba1714db3ae692de7bf3e435f1868c25e4b67a
Geekboy1011 <geekboy1011@gmail.com>
2018-04-01 15:21:06 -0400

    Objects are fun

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -37,10 +37,13 @@ class DomainList(npyscreen.MultiLineAction):
     def __init__(self, *args, **keywords):
         super(DomainList, self).__init__(*args, **keywords)
         self.add_handlers({
+            #curses.ascii.NL: self.h_exit_down,
             "^A": self.when_add_record,
             "^D": self.when_delete_record})
+
     def display_value(self,v1):
         return "[%s] %s" % (v1.id, v1.name)
+
     def when_add_record(self, *args, **keywords):
         self.parent.parentApp.switchForm('ADDDOMAIN')
         pass
@@ -54,8 +57,14 @@ class DomainList(npyscreen.MultiLineAction):
             commit()
             self.values = self.parent.update_domains()
 
+    @db_session
     def actionHighlighted(self, act_on_this, keypress):
-        pass
+
+        self.value = self._last_cursor_line
+        #self.set_is_line_important(self._my_widgets[self._last_cursor_line],True)
+        self.update()
+
+
 
 class UserList(npyscreen.MultiLineAction):
     def __init__(self, *args, **keywords):
@@ -63,6 +72,7 @@ class UserList(npyscreen.MultiLineAction):
         self.add_handlers({
             "^A": self.when_add_record,
             "^D": self.when_delete_record})
+
     def display_value(self,v1):
         return "[%s] %s" % (v1.id, v1.email)
 
@@ -75,3 +85,15 @@ class UserList(npyscreen.MultiLineAction):
                 form_color="DANGER")
     def actionHighlighted(self, act_on_this, keypress):
         pass
+
+class goodForm(InfoForm,npyscreen.FormBaseNew):
+    def buttonpress(self):
+        self.parentApp.switchFormPrevious()
+    def create(self):
+        y, x = self.curses_pad.getmaxyx()
+        y -= 5
+        self.myList.max_height = y
+        self.button = self.add(npyscreen.MiniButtonPress, use_max_space= True,
+                when_pressed_function = self.buttonpress, name = "OK", rely = -3,
+                relx = -12, cursor_color="CURSOR_INVERSE")
+
--- mailSys.py
+++ mailSys.py
@@ -10,6 +10,7 @@ class emailMain(npyscreen.FormBaseNew):
     def afterEditing(self):
 	pass
 	#self.parentApp.setNextForm(None)
+
     def create(self):
 	self.Title          = self.add(npyscreen.Textfield, value = "Select an Option:", name='Option')
 	self.myActions      = self.add(CustomWidgets.mainMenu, scoll_edit=True, name='Options:',
@@ -19,7 +20,8 @@ class emailMain(npyscreen.FormBaseNew):
 class emailUserForm(CustomWidgets.InfoForm,npyscreen.ActionPopup):
     user = None
     def afterEditing(self):
-        self.parentApp.setNextFormPrevious()
+        pass
+        #self.parentApp.setNextFormPrevious()
 
     def create(self):
        self.info = u''
@@ -34,16 +36,19 @@ class emailUserForm(CustomWidgets.InfoForm,npyscreen.ActionPopup):
                labelColor='CAUTION')
        self.myPassword     = self.add(npyscreen.TitleText, name = 'Password:')
        self.myDomain.add_handlers({
-           curses.ascii.NL: self.test,
-           " ": self.test})
+           curses.ascii.NL: self.h_key_handle,
+                       " ": self.h_key_handle})
        self.myEmail        = self.add(npyscreen.TitleMultiLine,
                scroll_exit = True, max_height=3, name='Configure:',
                values = self.value, rely=6)
 
     def on_cancel(self):
 	self.parentApp.setNextFormPrevious()
-    def test(self, *args, **keywords):
-        raise Exception("We did it")
+
+    def h_key_handle(self, *args, **keywords):
+        self.parentApp.setNextForm('DOMAINSELECT')
+        self.editing = False
+
 class addDomainPopup(npyscreen.ActionPopup):
     def create(self):
         self.myDomain = self.add(npyscreen.TitleText,name="New Domain")
@@ -53,10 +58,10 @@ class addDomainPopup(npyscreen.ActionPopup):
         if self.myDomain.value != '':
              Virtual_domains(name=self.myDomain.value)
              commit()
-        self.parentApp.setNextForm('DOMAIN')
+        self.parentApp.setNextFormPrevious()
     def on_cancel(self):
         self.myDomain.value = ''
-        self.parentApp.setNextForm('DOMAIN')
+        self.parentApp.setNextFormPrevious()
 
 class emailAliasPopup(npyscreen.ActionPopup):
     def beforeEditing(self):
@@ -66,37 +71,56 @@ class emailAliasPopup(npyscreen.ActionPopup):
     def afterEditing(self):
 	self.parentApp.setNextFormPrevious()
     def create(self):
-	self.myActions     = self.add(npyscreen.TitleMultiLine, scroll_edit=True, Name='Options',
+	self.myActions = self.add(npyscreen.TitleMultiLine, scroll_edit=True, Name='Options',
                 values = ['Add Alias', 'Delete alias'])
 
-class emailDomain(CustomWidgets.InfoForm,npyscreen.FormBaseNew):
+class emailDomain(CustomWidgets.goodForm):
     def beforeEditing(self):
-        self.myDomainList.values = self.update_domains()
+        self.myList.values = self.update_domains()
+
     @db_session
     def update_domains(self):
         return select(e for e in Virtual_domains)[:]
+
     def create(self):
         self.info = u" ^A: Add ── ^D: Delete "
-        y,x= self.curses_pad.getmaxyx()
-        y-=5
-        self.myDomainList = self.add(CustomWidgets.DomainList, scroll_edit = True,height=y)
-        self.button =       self.add(npyscreen.MiniButtonPress, use_max_space=True,
-                when_pressed_function=self.buttonpress, name='OK', rely=-3, relx=-10,
-                cursor_color='CURSOR_INVERSE')
+        self.myList = self.add(CustomWidgets.DomainList, scroll_edit = True)
+        super(emailDomain,self).create()
+
     def buttonpress(self):
         self.parentApp.switchForm('MAIN')
 
-class emailUser(CustomWidgets.InfoForm,npyscreen.Form):
+class emailDomainPopup(emailDomain):
+    #def create(self):
+    #    self.info = u" ^A: Add ── ^D: Delete "
+    #    self.myDomainList = self.add(CustomWidgets.DomainList,scroll_edit=True, name = 'Domains:')
+
+    def beforeEditing(self):
+        self.myList.values = self.update_domains()
+
+    @db_session
+    def update_domains(self):
+        return select(e for e in Virtual_domains)[:]
+
+    @db_session
+    def buttonpress(self):
+        if self.myList.value:
+            self.parentApp.getForm("USERFORM").myDomain.value=self.myList.values[self.myList.value].name
+        self.parentApp.switchFormPrevious()
+
+class emailUser(CustomWidgets.goodForm):
     @db_session
     def beforeEditing(self):
-        self.myUserList.values = select(e for e in Virtual_users)[:]
+        self.myList.values = select(e for e in Virtual_users)[:]
 
     def afterEditing(self):
-        self.parentApp.setNextFormPrevious()
+        #self.parentApp.setNextFormPrevious()
+        pass
 
     def create(self):
         self.info = u" ^A: Add ── ^D: Delete "
-        self.myUserList = self.add(CustomWidgets.UserList, scroll_edit = True)
+        self.myList = self.add(CustomWidgets.UserList, scroll_edit = True)
+        super(emailUser,self).create()
 
 class EmailManager(npyscreen.NPSAppManaged):
    def onStart(self):
@@ -106,6 +130,7 @@ class EmailManager(npyscreen.NPSAppManaged):
        self.addForm('MAIN', emailMain, name='Email Manager')
        self.addForm('DOMAIN', emailDomain, name='Domain Manager')
        self.addForm('ADDDOMAIN', addDomainPopup, name='Add Domain')
+       self.addForm('DOMAINSELECT', emailDomainPopup, name='Domain List')
        self.addForm("ALIASES", emailAliasPopup, name='Alias manager')
        self.addForm("USER", emailUser, name = 'User Manager')
        self.addForm("USERFORM", emailUserForm, name = 'Edit User')
