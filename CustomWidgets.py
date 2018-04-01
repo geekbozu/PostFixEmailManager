4e7a41a6b55873651d076c6165eeac6ce593a304
Geekboy1011 <geekboy1011@gmail.com>
2018-03-31 20:12:56 -0400

    More stuff

--- CustomWidgets.py
+++ CustomWidgets.py
@@ -1,11 +1,12 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 import npyscreen
 import curses
 
 class InfoForm(npyscreen.fmForm._FormBase):
     def __init__(self, info=None, *args, **keywords):
-        super(InfoForm,self).__init__(*args, **keywords)
         self.info=info
+        super(InfoForm,self).__init__(*args, **keywords)
 
     def display_menu_advert_at(self):
             return self.lines-1, 1
@@ -13,7 +14,7 @@ class InfoForm(npyscreen.fmForm._FormBase):
     def draw_form(self):
         super(InfoForm, self).draw_form()
         if self.info:
-            self.info = self.info.decode('utf-8', 'replace')
+           # self.info = self.info.decode('utf-8', 'replace')
             y, x = self.display_menu_advert_at()
             self.add_line(y, x,
               self.info,
@@ -31,4 +32,28 @@ class mainMenu(npyscreen.MultiLineAction):
             self.parent.parentApp.switchForm(None)
         pass
 
+class DomainList(npyscreen.MultiLineAction):
+    def __init__(self, *args, **keywords):
+        super(DomainList, self).__init__(*args, **keywords)
+        self.add_handlers({
+            "^A": self.when_add_record,
+            "^D": self.when_delete_record})
+    def when_add_record(self, act_on_this, keypress):
+        pass
+    def when_delete_record(self, act_on_this, keypress):
+        pass
+    def actionHighlighted(self, act_on_this, keypress):
+        pass
 
+class UserList(npyscreen.MultiLineAction):
+    def __init__(self, *args, **keywords):
+        super(UserList, self).__init__(*args, **keywords)
+        self.add_handlers({
+            "^A": self.when_add_record,
+            "^D": self.when_delete_record})
+    def when_add_record(self, act_on_this, keypress):
+        pass
+    def when_delete_record(self, act_on_this, keypress):
+        pass
+    def actionHighlighted(self, act_on_this, keypress):
+        pass
--- mailSys.py
+++ mailSys.py
@@ -1,6 +1,26 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
 import npyscreen
 import CustomWidgets
+from pony.orm import *
+
+class virtual_domains(db.entity):
+   id = primarykey(int, auto=true)
+   name = optional(str)
+
+class virtual_aliases(db.entity):
+    id = primarykey(int, auto=true)
+    domain_id = optional(int)
+    source = optional(str)
+    destination = optional(str)
+
+
+class virtual_users(db.entity):
+    id = primarykey(int, auto=true)
+    domain_id = optional(int)
+    password = optional(str)
+    email = optional(str)
+
 class emailMain(npyscreen.FormBaseNew):
     def afterEditing(self):
 	pass
@@ -11,13 +31,15 @@ class emailMain(npyscreen.FormBaseNew):
                 values = ['Manage Domains', 'Manage Users', 'Quit'],
                 relx = 6)
 
-
-
 class emailUserForm(CustomWidgets.InfoForm,npyscreen.ActionForm):
     def afterEditing(self):
         self.parentApp.setNextFormPrevious()
+    def beforeEditing(self):
+        pass
+    #Need to Pull SQL info here for user
 
     def create(self):
+       self.value=None
        self.myTitle        = self.add(npyscreen.TitleText, name='User Email:', editable=False)
        self.myDomain       = self.add(npyscreen.TitleText, name='Domain:', color= 'CAUTIONHL',
                labelColor='CAUTION', value='test', editable=False)
@@ -28,6 +50,10 @@ class emailUserForm(CustomWidgets.InfoForm,npyscreen.ActionForm):
 	self.parentApp.setNextFormPrevious()
 
 class emailAliasPopup(npyscreen.ActionPopup):
+    def beforeEditing(self):
+        pass
+    #load Aliases for selected user here
+
     def afterEditing(self):
 	self.parentApp.setNextFormPrevious()
     def create(self):
@@ -35,19 +61,39 @@ class emailAliasPopup(npyscreen.ActionPopup):
                 values = ['Add Alias', 'Delete alias'])
 
 class emailDomain(CustomWidgets.InfoForm,npyscreen.Form):
+    def beforeEditing(self):
+        pass
+    #pull Domain List here
+
     def afterEditing(self):
         self.parentApp.setNextFormPrevious()
     def create(self):
-        self.info = "^A: Add | ^D: Delete"
-        self.myDomainList = self.add(npyscreen.MultiLineAction, scroll_edit = True,
+        self.info = u" ^A: Add ── ^D: Delete "
+        self.myDomainList = self.add(CustomWidgets.DomainList, scroll_edit = True,
                 values = [chr(i) for i in range(21,81)])
+
+class emailUser(CustomWidgets.InfoForm,npyscreen.Form):
+    def beforeEditing(self):
+        pass
+    #pull User list here
+
+    def afterEditing(self):
+        self.parentApp.setNextFormPrevious()
+
+    def create(self):
+        self.value = None
+        self.info = u" ^A: Add ── ^D: Delete "
+        self.myUserList = self.add(CustomWidgets.UserList, scroll_edit = True,
+                values = self.value)
 
 class EmailManager(npyscreen.NPSAppManaged):
    def onStart(self):
+       self.db = database()
+       db.generate_mapping()
        self.addForm('MAIN', emailMain, name='Email Manager')
        self.addForm('DOMAIN', emailDomain, name='Domain Manager')
        self.addForm("ALIASES", emailAliasPopup, name='Alias manager')
-       self.addForm("USER", emailUserForm, name = 'User Manager')
+       self.addForm("USER", emailUser, name = 'User Manager')
        # A real application might define more forms here.......
 
 if __name__ == '__main__':
